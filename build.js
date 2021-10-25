055f6f61e93f417b80525ddb2715c73c6a020148
Boris Brue <boris@zuckersalzundpfeffer.de>
2021-10-25 22:35:07 +0200

    foo

--- build.js
+++ build.js
@@ -1,2 +1,17 @@
+const spawn = require('child_process').spawn;
+
 const nuxt = require('@nuxt/cli')
-nuxt.run(['build'])
+const build = nuxt.run(['build'])
+build.then(() => {
+  process.on('exit', () => {
+    const child = spawn('node', ['./app.js'], {
+      detached: true,
+      stdio: 'ignore'
+   });
+ });
+}).finally(() => {
+  process.exit(1)
+})
+
+
+//
